d7c5e4eadcc6311a573a35bc643b8805654b86c2
Say <sktjdgns1189@naver.com>
2015-01-22 14:43:08 +0900

    modules.sh: change gcc version

--- modules.sh
+++ modules.sh
@@ -1,3 +1,3 @@
 rm -rf ../Modules/*
 find -name '*.ko' -exec cp -av {} ../Modules \;
-        for i in ../Modules/*; do /opt/toolchains/arm-eabi-4.7/bin/arm-eabi-strip --strip-unneeded $i;done;\
+        for i in ../Modules/*; do /opt/toolchains/arm-eabi-4.8/bin/arm-eabi-strip --strip-unneeded $i;done;\
